4094e2e859151c01884e8604edbb88bbb2f9e031
xiaozhongkai <xiaozhongkai.0423@cashbus.com>
2019-10-16 09:48:42 +0800

    2.Vue.js通过axios访问flask RESTful API

---
+++ front-end/src/components/Ping.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button type="button" class="btn btn-primary">{{ msg }}</button>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    export default {
+    name: 'Ping',
+    data(){
+        return {
+            msg: ""
+        }
+    },
+    methods: {
+        getMessage() {
+            const path = "http://localhost:5000/api/ping"
+            axios.get(path)
+            .then((res) => {
+                this.msg = res.data
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+        }
+    },
+    created () {
+        this.getMessage()
+    }
+}
+</script>
